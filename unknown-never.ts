8b7e92ce87989f75bc8104fe7d0c56caf7c1994e
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-30 20:43:58 +0600

    Unknown and never types added

---
+++ unknown-never.ts
@@ -0,0 +1,20 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Haris';
+
+if(typeof userInput === 'string') {
+	userName = userInput;
+}
+
+
+
+function generateError(message: string, code: number): never {
+	throw {
+		message: message,
+		errorCode: code
+	};
+}
+
+generateError('An error occured!', 5000)
